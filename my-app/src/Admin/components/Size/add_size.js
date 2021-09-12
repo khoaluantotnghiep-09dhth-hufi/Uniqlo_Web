e4446bcd9446508113101a6c44afb57feb0ae0b3
Tạ Quang Trung <76809187+taquangtrung115@users.noreply.github.com>
2021-09-12 20:48:05 +0700

    update - admin

---
+++ my-app/src/Admin/components/Size/add_size.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+    CForm,
+    CLabel,
+    CContainer,
+    CInput,
+    CCol,
+    CRow,
+    CFormGroup,
+    CButton,
+
+} from '@coreui/react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faPlus,
+
+} from "@fortawesome/free-solid-svg-icons";
+export default class addColor extends React.Component {
+    render() {
+        return (
+            <CContainer fluid>
+                <CRow>
+                    <CCol sm="12">
+                        <CForm action="" method="post">
+                            <CFormGroup>
+                                <CLabel htmlFor="exampleFormControlInput1">Tên Kích Cỡ</CLabel>
+                                <CInput
+                                    type="text"
+                                    id="txtName"
+                                    name="txtName"
+                                    placeholder="Tên kích cỡ..."
+                                    autoComplete="name"
+                                />
+                            </CFormGroup>
+                           
+                            <CFormGroup >
+                                <CButton color='danger' className="m-2" >  <FontAwesomeIcon icon={faPlus} className="mr-2" size="lg" /> Thêm</CButton>
+
+                            </CFormGroup>
+                        </CForm>
+                    </CCol>
+                </CRow>
+            </CContainer>
+        )
+    }
+}
