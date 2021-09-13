b72ee4ba46ad72d66260d96277f1ba684cc7da17
Tạ Quang Trung <76809187+taquangtrung115@users.noreply.github.com>
2021-09-14 01:35:00 +0700

    load đối tượng

--- my-app/src/Admin/containers/_nav.js
+++ my-app/src/Admin/containers/_nav.js
@@ -31,10 +31,10 @@ import {
 const _nav =  [
   {
     _tag: 'CSidebarNavItem',
-    name: <h4>Trang Chủ</h4>,
+    name: 'Trang Chủ',
      to: '/admin',
     // icon: <Fon name="cil-home" customClasses="c-sidebar-nav-icon"/>,
-    icon: <FontAwesomeIcon icon={faStore} className="mr-2" size="2x"/>
+    icon: <FontAwesomeIcon icon={faStore} className="mr-2"/>
  
   },
   {
@@ -43,25 +43,25 @@ const _nav =  [
   },
   {
     _tag: 'CSidebarNavDropdown',
-    name: <h5>Đơn Hàng</h5>,
+    name: 'Đơn Hàng',
     route: '/admin/system/order',
     icon: <FontAwesomeIcon icon={faTasks} className="mr-2" />,
     _children: [
       {
         _tag: 'CSidebarNavItem',
-        name:  <h5>Tất Cả</h5>,
+        name:  'Tất Cả',
         to: '/admin/system/order/all',
         icon: <FontAwesomeIcon icon={faListUl} className="mr-2" />,
       },
       {
         _tag: 'CSidebarNavItem',
-        name:  <h5>Đã Xác Nhận</h5>,
+        name:  'Đã Xác Nhận',
         to: '/admin/system/order/confirmed',
         icon: <FontAwesomeIcon icon={faCheck} className="mr-2" />,
       },
       {
         _tag: 'CSidebarNavItem',
-        name: <h5>Chưa Xác Nhận</h5>,
+        name: 'Chưa Xác Nhận',
         to: '/admin/system/order/unconfirmed',
         
         icon: <FontAwesomeIcon icon={faExclamationCircle} className="mr-2" />,
@@ -72,14 +72,14 @@ const _nav =  [
   },
   {
     _tag: 'CSidebarNavItem',
-    name:  <h5>Tin Tức</h5>,
+    name:  'Tin Tức',
     to: '/admin/system/news',
     icon: <FontAwesomeIcon icon={faNewspaper} className="mr-2" />,
     
   },
   {
     _tag: 'CSidebarNavItem',
-    name:  <h5>Khuyến Mãi</h5>,
+    name:  'Khuyến Mãi',
     to: '/admin/system/discount',
     icon: <FontAwesomeIcon icon={faPercent} className="mr-2" />,
     
@@ -90,32 +90,32 @@ const _nav =  [
   },
   {
     _tag: 'CSidebarNavItem',
-    name:  <h5>Thống Kê Doanh Số</h5>,
+    name:  'Thống Kê Doanh Số',
     to: '/admin/manage/statistical',
     icon: <FontAwesomeIcon icon={faChartLine} className="mr-2" />,
     
   },
   {
     _tag: 'CSidebarNavDropdown',
-    name:  <h5>Nhập/Xuất Kho</h5>,
+    name:  'Nhập/Xuất Kho',
     route: '/admin/manage',
     icon: <FontAwesomeIcon icon={faWarehouse} className="mr-2" />,
     _children: [
       {
         _tag: 'CSidebarNavItem',
-        name:  <h5>Đặt Hàng</h5>,
+        name:  'Đặt Hàng',
         to: '/admin/manage/order-product',
         icon: <FontAwesomeIcon icon={faPaperPlane} className="mr-2"  />,
       },
       {
         _tag: 'CSidebarNavItem',
-        name:  <h5>Nhập Hàng</h5>,
+        name:  'Nhập Hàng',
         to: '/admin/manage/import-product',
         icon: <FontAwesomeIcon icon={faFileImport} className="mr-2" />,
       },
       {
         _tag: 'CSidebarNavItem',
-        name:  <h5>Thống Kê</h5>,
+        name:  'Thống Kê',
         to: '/admin/manage/statistical-order-import',
         icon: <FontAwesomeIcon icon={faChartBar} className="mr-2" />,
       },
@@ -124,25 +124,25 @@ const _nav =  [
   },
   {
     _tag: 'CSidebarNavDropdown',
-    name:  <h5>Danh Sách</h5>,
+    name:  'Danh Sách',
     route: '/admin/manage/cate',
     icon: <FontAwesomeIcon icon={faGripHorizontal} className="mr-2" />,
     _children: [
       {
         _tag: 'CSidebarNavItem',
-        name:  <h5>Đối Tượng</h5>,
+        name:  'Đối Tượng',
         to: '/admin/manage/objects',
         icon: <FontAwesomeIcon icon={faBars} className="mr-2" />,
       },
       {
         _tag: 'CSidebarNavItem',
-        name:  <h5>Loại Sản Phẩm</h5>,
+        name:  'Loại Sản Phẩm',
         to: '/admin/manage/sectors',
         icon: <FontAwesomeIcon icon={faGripLines} className="mr-2" />,
       },
       {
         _tag: 'CSidebarNavItem',
-        name:  <h5>Danh Mục</h5>,
+        name:  'Danh Mục',
         to: '/admin/manage/categories',
         icon: <FontAwesomeIcon icon={faMinus} className="mr-2" />,
       },
@@ -150,31 +150,31 @@ const _nav =  [
   },
   {
     _tag: 'CSidebarNavItem',
-    name:  <h5>Sản Phẩm</h5>,
+    name:  'Sản Phẩm',
     to: '/admin/manage/products',
     icon: <FontAwesomeIcon icon={faTshirt} className="mr-2" size="lg"/>,
   },
   {
     _tag: 'CSidebarNavItem',
-    name:  <h5>Màu</h5>,
+    name: 'Màu',
     to: '/admin/manage/color',
     icon: <FontAwesomeIcon icon={faPalette} className="mr-2" />,
   },
   {
     _tag: 'CSidebarNavItem',
-    name:  <h5>Kích Cỡ</h5>,
+    name:  'Kích Cỡ',
     to: '/admin/manage/size',
     icon: <FontAwesomeIcon icon={faDraftingCompass} className="mr-2" />,
   },
   {
     _tag: 'CSidebarNavItem',
-    name:  <h5>Nhân Viên</h5>,
+    name: 'Nhân Viên',
     to: '/admin/manage/staffs',
     icon: <FontAwesomeIcon icon={faIdCard} className="mr-2" />,
   },
   {
     _tag: 'CSidebarNavItem',
-    name:  <h5>Khách Hàng</h5>,
+    name: 'Khách Hàng',
     to: '/admin/manage/customers',
     icon: <FontAwesomeIcon icon={faUsers} className="mr-2" />,
   },
@@ -184,7 +184,7 @@ const _nav =  [
   },
   {
     _tag: 'CSidebarNavItem',
-    name: <h5>Phân Quyền</h5>,
+    name: 'Phân Quyền',
     to: '/admin/account',
     icon: <FontAwesomeIcon icon={faUserCog} className="mr-2" />,
     
--- my-app/src/Admin/reducer/product.js
+++
@@ -1,7 +0,0 @@
-var initialState = [];
-const products = (state = initialState, action) => {
-    switch (action.type){
-        default: return [...state];
-    }
-};
-export default products;
