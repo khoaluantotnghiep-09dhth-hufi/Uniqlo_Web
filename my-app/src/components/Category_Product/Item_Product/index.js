efeb8f7f5b1a4a7ad133dfe89ae2bab1020a31ab
Nguyễn Thanh Huy <54466996+YoungBirds134@users.noreply.github.com>
2021-09-16 15:26:04 +0700

    -Sửa Sản Phẩm

--- my-app/src/components/Category_Product/Item_Product/index.js
+++ my-app/src/components/Category_Product/Item_Product/index.js
@@ -4,18 +4,60 @@ import { Card, Button } from "react-bootstrap";
 class index extends Component {
   render() {
     var { product } = this.props;
-    // product.desciption==='' && 
-    var elementSale= product.desciption==='0'?'':  <span class="badge badge-danger" style={{transform: "translateX(190px)"}}>{product.desciption}<i class="fas fa-percent " aria-hidden="true"></i></span>;
+    // product.desciption==='' &&
+    const formatter = new Intl.NumberFormat("vi-VN", {
+      style: "currency",
+      currency: "VND",
+      minimumFractionDigits: 0,
+    });
+    var elementSale =
+      product.desciption === "0" ? (
+        ""
+      ) : (
+        <span
+          class="badge badge-danger"
+          style={{ transform: "translateX(190px)" }}
+        >
+          {product.desciption}
+          <i class="fas fa-percent " aria-hidden="true"></i>
+        </span>
+      );
+
+    if (product.desciption) {
+      var newPrice = (parseInt(product.desciption) / 100) * product.price;
+    }
+
+    var elementNewPrice = newPrice ? (
+      <span
+        style={{ marginLeft: "40px", color: "red" }}
+        className="font-weight-bold "
+      >
+        {" "}
+        {formatter.format(newPrice)}{" "}
+      </span>
+    ) : (
+      ""
+    );
+
+    var elementPrice = elementNewPrice ? (
+      <span>
+        {" "}
+        <del>{formatter.format(product.price)} </del>
+      </span>
+    ) : (
+      <span className="text-center"> {formatter.format(product.price)}</span>
+    );
+
     return (
       <Card>
         <Card.Img variant="top" src={product.image} />
-      {elementSale}
+        {elementSale}
         <Card.Body>
           <Card.Title>{product.name_product}</Card.Title>
 
           <Card.Text className="text-left">
-            <span> <del>{product.price} VND</del></span>
-            <span style={{marginLeft:'40px',color:'red'}} className="font-weight-bold"> {product.price} VND</span>
+            {elementPrice}
+            {elementNewPrice}
           </Card.Text>
 
           <Button
