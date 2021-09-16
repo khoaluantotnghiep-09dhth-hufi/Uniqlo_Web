6bb123dd14aad8e40d7b1e0f3cf37b930285035b
Nguyễn Thanh Huy <54466996+YoungBirds134@users.noreply.github.com>
2021-09-16 12:14:56 +0700

    - Quan Lý Đơn Hàng

--- my-app/src/Admin/components/Order/unconfimredOrder.js
+++ my-app/src/Admin/components/Order/unconfimredOrder.js
@@ -1,68 +1,120 @@
-import React from 'react'
+import React from "react";
+import { connect } from "react-redux";
+import * as actions from "./../../../actions/index";
+import { Alert } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faTimes } from "@fortawesome/free-solid-svg-icons";
 import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CDataTable,
+  CRow,
+} from "@coreui/react";
 
-    CCard,
-    CCardBody,
-    CCardHeader,
-    CCol,
-    CDataTable,
-    CRow
-} from '@coreui/react'
-
-
-import usersData from '../User/UserData';
-
-// const getBadge = status => {
-//     switch (status) {
-//         case 'Active': return 'success'
-//         case 'Inactive': return 'secondary'
-//         case 'Pending': return 'warning'
-//         case 'Banned': return 'danger'
-//         default: return 'primary'
-//     }
-// }
-const fields = ['name', 'registered', 'role', 'status']
+const fields = [
+  "index",
+  "id",
+  "date",
+  "address",
+  "email",
+  "phone",
+  "total",
+  "status",
+  "Hành Động",
+];
 
 class OrderUnConfirmedOrder extends React.Component {
-    render() {
-        return (
-            <>
-
-                <CRow>
-                    <CCol xs="12" lg="24">
-                        <CCard>
-                            <CCardHeader>
-                                Danh Sách Đơn Hàng Chưa Xác Nhận
-                            </CCardHeader>
-                            <CCardBody>
-                                <CDataTable
-                                    items={usersData}
-                                    fields={fields}
-                                    itemsPerPage={8}
-                                    pagination
-                                    scopedSlots={{
-                                        'status':
-                                            (item) => (
-                                                <td>
-                                                    {/* <CBadge color={getBadge(item.status)}>
-                                                        {item.status}
-                                                    </CBadge> */}
-                                                </td>
-                                            )
-
-                                    }}
-                                />
-                            </CCardBody>
-                        </CCard>
-                    </CCol>
-
-
-                </CRow>
-
-
-            </>
-        )
+  componentDidMount() {
+    this.props.fetchBills();
+  }
+  onDeleteBill = (item) => {
+    this.props.onDeleteItemBill(item);
+  };
+  getBadge = (status) => {
+    switch (status) {
+      case 0:
+        return "success";
+      case 1:
+        return "danger";
+      default:
+        return "primary";
     }
-}
+  };
+  render() {
+    var { bill } = this.props;
 
-export default OrderUnConfirmedOrder
+    var dataBill = bill
+      .filter((bill) => bill.status === 0)
+      .map((item, index) => {
+        return { ...item, index };
+      });
+    return (
+      <>
+        <CRow>
+          <CCol xs="12" lg="24">
+            <CCard>
+              <CCardHeader>Danh Sách Đơn Hàng Chưa Xác Nhận</CCardHeader>
+              <CCardBody>
+                <CDataTable
+                  items={dataBill}
+                  fields={fields}
+                  itemsPerPage={8}
+                  pagination
+                  scopedSlots={{
+                    status: (item) => (
+                      <td>
+                        <Alert variant={this.getBadge(item.status)}>
+                          {item.status === 0 ? "Đã Xác Nhận" : "Chưa Xác Nhận"}
+                        </Alert>
+                      </td>
+                    ),
+                    "Hành Động": (item) => (
+                      <td>
+                        <CButton
+                          type="button"
+                          className="btn btn-warning"
+                          onClick={() => {
+                            this.onDeleteBill(item.id);
+                          }}
+                        >
+                          <FontAwesomeIcon
+                            icon={faTimes}
+                            className="mr-2"
+                            size="lg"
+                          />
+                          Xóa
+                        </CButton>
+                      </td>
+                    ),
+                  }}
+                />
+              </CCardBody>
+            </CCard>
+          </CCol>
+        </CRow>
+      </>
+    );
+  }
+}
+var mapStateToProps = (state) => {
+  return {
+    bill: state.bill,
+  };
+};
+var mapDispatchToProps = (dispatch, props) => {
+  return {
+    fetchBills: () => {
+      return dispatch(actions.fetchBillResquest());
+    },
+    onDeleteItemBill: (id) => {
+      return dispatch(actions.onDeleteBillResquest(id));
+    },
+  };
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(OrderUnConfirmedOrder);
