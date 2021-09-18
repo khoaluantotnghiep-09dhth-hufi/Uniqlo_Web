7f95240748c9a2e6d8a5b2e42306130780c8bce4
Nguyễn Thanh Huy <54466996+YoungBirds134@users.noreply.github.com>
2021-09-18 12:54:33 +0700

    - Tranf CTSP

--- my-app/src/components/Detail_Product/index.js
+++ my-app/src/components/Detail_Product/index.js
@@ -4,53 +4,108 @@ import { connect } from "react-redux";
 import * as actions from "./../../actions/productActions";
 class index extends Component {
   componentDidMount() {
-    
     this.props.onGetAllProduct();
   }
 
-  showDetailProduct = (products, id_product) => {
+  showDetailProduct = (products_category, id_product) => {
+    const formatter = new Intl.NumberFormat("vi-VN", {
+      style: "currency",
+      currency: "VND",
+      minimumFractionDigits: 0,
+    });
+
+    var result = null;
+    result = products_category
+      .filter((product) => product.id === id_product)
+
+      .map((product, index) => {
+        console.log(product.percentSale);
+        if (product.percentSale) {
+          var newPrice = (parseInt(product.percentSale) / 100) * product.price;
+        }
+        var elementNewPrice = newPrice ? (
+          <span
+          // style={{ marginLeft: "40px", color: "red" }}
+          // className="font-weight-bold "
+          >
+            {" "}
+            {formatter.format(newPrice)}{" "}
+          </span>
+        ) : (
+          ""
+        );
+
+        var elementPrice = elementNewPrice ? (
+          <span>
+            {" "}
+            <del>{formatter.format(product.price)} </del>
+          </span>
+        ) : (
+          <span> {formatter.format(product.price)}</span>
+        );
+        return (
+          <React.Fragment>
+            <h4 className="text-center mb-4">CHI TIẾT SẢN PHẨM</h4>
+            <Col lg="3" style={{ backgroundColor: "red" }}>
+              <Row>
+                <Col>
+                  <h3>{product.name}</h3>
+                </Col>
+              </Row>
+              <Row>
+                <Col>{elementPrice}</Col>
+              </Row>
+              <Row>
+                <Col>{elementNewPrice}</Col>
+              </Row>
+            </Col>
+            <Col lg="6">
+              <Image src={product.image}></Image>
+            </Col>
+          </React.Fragment>
+        );
+      });
+
+    return result;
+  };
+
+  ShowColorAndSize = (products, id_product) => {
     var result = null;
     result = products
       .filter((product) => product.id === id_product)
       .map((product, index) => {
         return (
-          <Container >
-            <Row className="mt-4">
-              <h4 className="text-center mb-4">CHI TIẾT SẢN PHẨM</h4>
-              <Col lg="3" style={{ backgroundColor: "red" }}>
-                <Row>
-                  <Col>
-                    <h3>{product.name}</h3>
-                  </Col>
-                  <Col>{product.price}</Col>
-                </Row>
-              </Col>
-              <Col lg="6">
-                <Image src={product.image}></Image>
-              </Col>
-
-              <Col lg="3" style={{ backgroundColor: "red" }}>
-                <Row>
-                  <Col>
-                    
-                  </Col>
-                  <Col>299.000VND</Col>
-                </Row>
+          <React.Fragment>
+            <Row>
+              <Col>
+                <h3>doan 1</h3>
               </Col>
             </Row>
-          </Container>
+            <Row>
+              <Col>
+                <h3>doan 2</h3>
+              </Col>
+            </Row>
+          </React.Fragment>
         );
       });
     return result;
   };
   render() {
-    var { match, products } = this.props;
+    var { match, products, products_category } = this.props;
 
     var id_product = match.params.id_product;
 
     return (
       <React.Fragment>
-        {this.showDetailProduct(products, id_product)}
+        <Container>
+          <Row className="mt-4">
+            {this.showDetailProduct(products_category, id_product)}
+            <Col lg="3" style={{ backgroundColor: "red" }}>
+              {this.ShowColorAndSize(products, id_product)}
+            </Col>
+          </Row>
+        </Container>
       </React.Fragment>
     );
   }
@@ -58,6 +113,7 @@ class index extends Component {
 var mapStateToProps = (state) => {
   return {
     products: state.products,
+    products_category: state.products_category,
   };
 };
 var mapDispatchToProps = (dispatch, props) => {
