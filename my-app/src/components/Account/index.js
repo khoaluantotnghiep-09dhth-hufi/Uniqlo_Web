f24e009891535d860639ed84dd104af5fa913b10
Nguyễn Thanh Huy <54466996+YoungBirds134@users.noreply.github.com>
2021-09-23 15:03:46 +0700

    - Chức Năng Sau Khi Login

--- my-app/src/components/Account/index.js
+++ my-app/src/components/Account/index.js
@@ -1,28 +1,73 @@
 import React, { Component } from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
-import Table from "./Table/index";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Modal,
+  FloatingLabel,
+  Form,
+} from "react-bootstrap";
+import Moment from "react-moment";
+
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import * as actions from "./../../actions/index";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faPlus, faTimes, faTools } from "@fortawesome/free-solid-svg-icons";
+import { CButton, CDataTable } from "@coreui/react";
+const formatter = new Intl.NumberFormat("vi-VN", {
+  style: "currency",
+  currency: "VND",
+  minimumFractionDigits: 0,
+});
+
+const fields = [
+  "STT",
+  { key: "order_date", label: "Ngày Đặt" },
+  { key: "delivery_date", label: "Ngày Giao" },
+  { key: "total", label: "Giá Đơn hàng" },
+  { key: "total_quantity", label: "Số Lượng" },
+  { key: "status", label: "Tình Trạng" },
+  "Hành Động",
+];
 class index extends Component {
   constructor(props) {
     super(props);
     this.state = {
       isCheckSignOut: false,
+      show: false,
+      setShow: false,
     };
   }
+  componentDidMount() {
+    this.props.fetchBillsCustomer();
+  }
+  handleClose = () => {
+    this.setState({
+      show: !true,
+    });
+  };
+  handleShow = () => {
+    this.setState({
+      show: !false,
+    });
+  };
   onSignOut = () => {
-    var { cart} = this.props;
+    var { cart } = this.props;
     sessionStorage.clear("user");
     localStorage.clear();
-    
+
     this.props.onResetCart(cart);
 
     this.setState({ isCheckSignOut: true });
   };
+
   render() {
-  
-    var sessionUser=JSON.parse(sessionStorage.getItem('user'))
+    var { bills_customer } = this.props;
+    var { show } = this.state;
+    var sessionUser = JSON.parse(sessionStorage.getItem("user"));
     console.log(sessionUser);
     var { isCheckSignOut } = this.state;
     if (isCheckSignOut) {
@@ -45,7 +90,69 @@ class index extends Component {
             </Row>
             <Row>
               <Col>
-                <Table phoneUser={sessionUser.phone}/>
+                <CDataTable
+                  items={bills_customer}
+                  fields={fields}
+                  itemsPerPage={5}
+                  hover
+                  sorter
+                  pagination
+                  scopedSlots={{
+                    status: (item) => (
+                      <td>
+                        {/* <CBadge color={getBadge(item.status)}>
+                                                        {item.status}
+                                                    </CBadge> */}
+                      </td>
+                    ),
+                  }}
+                  scopedSlots={{
+                    total: (item) => <td>{formatter.format(item.total)}</td>,
+                    status: (item) => (
+                      <td>{item.status === 0 ? "Chưa Giao" : "Đã Giao"}</td>
+                    ),
+                    order_date: (item) => (
+                      <td>
+                        <Moment format="DD/MM/YYYY">{item.order_date}</Moment>
+                      </td>
+                    ),
+
+                    delivery_date: (item) => (
+                      <td>
+                        {item.delivery_date ? (
+                          <Moment format="DD/MM/YYYY">
+                            {item.delivery_date}
+                          </Moment>
+                        ) : (
+                          <p>Chờ Xác Nhận</p>
+                        )}
+                      </td>
+                    ),
+                    "Hành Động": (item) => (
+                      <td>
+                        {item.status === 0 ? (
+                          <Button
+                            type="button"
+                            variant="outline-secondary"
+                            size="sm"
+                            style={{ margin: 0 }}
+                            onClick={this.handleShow}
+                          >
+                            <FontAwesomeIcon
+                              icon={faTimes}
+                              className="mr-2"
+                              size="sm"
+                            />
+                            <small> Hủy Đơn</small>
+                          </Button>
+                        ) : (
+                          ""
+                        )}
+                      </td>
+                    ),
+                    STT: (item, index) => <td>{index + 1}</td>,
+                  }}
+                />
               </Col>
             </Row>
           </Col>
@@ -78,6 +185,32 @@ class index extends Component {
             </Row>
           </Col>
         </Row>
+        <Modal show={show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Yêu Cầu Hủy Đơn Hàng</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+          <Form>
+            <FloatingLabel
+              controlId="floatingTextarea"
+              label="Xin Mời Nhập Lý Do"
+              className="mb-3"
+              required 
+            >
+              <Form.Control as="textarea" placeholder="Leave a comment here" maxlenght='100' required  autofocus/>
+            </FloatingLabel>
+            <Button type="submit" variant="outline-secondary">
+              Gửi Yêu Cầu
+            </Button>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="outline-secondary" onClick={this.handleClose}>
+              Đóng
+            </Button>
+            
+          </Modal.Footer>
+        </Modal>
       </Container>
     );
   }
@@ -85,12 +218,16 @@ class index extends Component {
 var mapStateToProps = (state) => {
   return {
     cart: state.cart,
+    bills_customer: state.bills_customer,
   };
 };
 var mapDispatchToProps = (dispatch, props) => {
   return {
     onResetCart: (product) => {
       dispatch(actions.onRestCart(product));
+    },
+    fetchBillsCustomer: () => {
+      dispatch(actions.fetchBillsCustomerResquest());
     },
   };
 };
