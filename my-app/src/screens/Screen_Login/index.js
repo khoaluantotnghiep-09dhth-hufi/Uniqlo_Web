62870db7198df8a4b627bf0824a2ca60fddfa2b6
lqt2k <wintrung01@gmail.com>
2021-09-24 17:13:18 +0700

    template chart - npm install @coreui/react-chartjs

--- my-app/src/screens/Screen_Login/index.js
+++ my-app/src/screens/Screen_Login/index.js
@@ -7,11 +7,8 @@ class Screen_Account extends Component {
     render() {
         var{location} = this.props;
         return (
-            <div >
-              
-               
+            <div >             
                 <Login location={location}/>
-           
             </div>
 
         );
