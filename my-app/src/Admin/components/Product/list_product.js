7a3de0093b71c20820af88e7e32d2da3a7d208f0
Tạ Quang Trung <76809187+taquangtrung115@users.noreply.github.com>
2021-10-02 20:02:16 +0700

    Update list_product.js

--- my-app/src/Admin/components/Product/list_product.js
+++ my-app/src/Admin/components/Product/list_product.js
@@ -19,34 +19,73 @@ import { Link } from 'react-router-dom';
 import { connect } from "react-redux";
 import * as actions from "./../../../actions/productAdminActions";
 import { Image } from 'react-bootstrap';
-import ConvertIMG from '../../utils/getBase64';
-
 const fields = [
-    'STT',
-    { key: 'id', label: 'Mã' },
-    { key: 'name', label: 'Tên' },
-    { key: 'price', label: 'Giá' },
-    { key: 'description', label: 'Mô Tả' },
-    { key: 'like_product', label: 'Lượt Thích' },
-    { key: 'dislike_product', label: 'Không Thích' },
-    { key: 'nameCategory', label: 'Danh Mục' },
-    { key: 'namePromotion', label: 'Khuyến Mãi' },
-    { key: 'image', label: 'Ảnh' },
-    // { key: 'quantityAllProduct', label: 'Số Lượng' },
-    // { key: 'nameColor', label: 'Màu' },
-    // { key: 'nameSize', label: 'Kích Cỡ' },
-    'Thao Tác',
-]
-const getBadge = color => {
-    switch (color) {
-        case "Red": return 'danger'
-        case "Blue": return 'primary'
-        case "Pink": return 'dark'
-        case "Orange": return 'warning'
-        default: return 'white'
+    {
+        key: 'STT',
+        label: 'STT',
+        _style: { width: '1%' },
+        sorter: false,
+        filter: false
+    },
+    {
+        key: 'id',
+        label: 'Mã',
+        _style: { width: '1%' },
+        sorter: false,
+        filter: false
+    },
+    {
+        key: 'name',
+        label: 'Tên',
+        _style: { width: '15%' },
+    },
+    {
+        key: 'price',
+        label: 'Giá',
+        _style: { width: '1%' },
+    },
+    {
+        key: 'description',
+        label: 'Mô Tả',
+        _style: { width: '11%' },
+        sorter: false,
+        filter: false
+    },
+    {
+        key: 'like_product',
+        label: 'Lượt Thích',
+        _style: { width: '1%' },
+        filter: false
+    },
+    {
+        key: 'dislike_product',
+        label: 'Không Thích',
+        _style: { width: '1%' },
+        filter: false
+    },
+    {
+        key: 'nameCategory',
+        label: 'Danh Mục',
+        _style: { width: '1%' },
+    },
+    {
+        key: 'namePromotion',
+        label: 'Khuyến Mãi',
+        _style: { width: '1%' },
+    },
+    {
+        key: 'image',
+        label: 'Ảnh',
+        filter: false
+    },
+    {
+        key: 'Thao Tác',
+        label: 'Thao Tác',
+        _style: { width: '1%' },
+        sorter: false,
+        filter: false
     }
-}
-
+]
 class ListProducts extends React.Component {
     componentDidMount() {
         this.props.fetchProducts();
@@ -63,7 +102,6 @@ class ListProducts extends React.Component {
         });
         return (
             <>
-                {/* <Route to="/admin/system/discount/add"><CButton className="btn btn-danger" >Thêm Khuyến Mãi Mới</CButton></Route> */}
                 <Link to="/admin/manage/product/add">
                     <CButton type="button" className="btn btn-danger">
                         <FontAwesomeIcon icon={faPlus} className="mr-2" size="lg" />Thêm Mới
@@ -134,15 +172,6 @@ class ListProducts extends React.Component {
                                                     <Image style={{ width: "200px", height: "200px" }} src={item.image} thumbnail />
                                                 </td>
                                             ),
-                                        // "nameColor": (item) => (
-                                        //     <td>
-                                        //         <Alert variant={getBadge(item.nameColor)}>
-                                        //             {/* {item.status === 0 ? 'Chưa Giao' : 'Đã Giao'} */}
-                                        //         </Alert>
-
-
-                                        //     </td>
-                                        // ),
                                     }}
                                 />
                             </CCardBody>
